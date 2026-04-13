bf8f1fd5536accfb9e763780bdf963551671fb1e
Ayesha Rafi <ayesharafi761@gmail.com>
2026-04-13 19:47:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ var ecommerce = {
             vaseline: {
                 "Lip Balm": {
                     name: "Vaseline® Lip Therapy® Original Stick",
-                    src: ".img/vaseline lipbalm.jpg",
+                    src: "img/vaseline lipbalm.jpg",
                     price: "100",
                     description: "Made with Vaseline® Jelly, it provides essential moisturization to the lips, locking in moisture, and making your lips look healthier and feel softer."
                 },
                 "lotion": {
                     name: "Vaseline® coco butter Lotion",
-                    src: ".img/vaseline lotion.jpg",
+                    src: "img/vaseline lotion.jpg",
                     price: "1500",
                     description: "Deep Hydration: Formulated with Ultra-Hydrating Lipids to strengthen the skin barrier and lock in moisture."
                 },
                 "Body oil":{
                     name:"Veseline body oil",
-                    src:".img/vesline oil.jpg",
+                    src:"img/vesline oil.jpg",
                     price:"6000",
                     description:"Absorbs quickly, providing a radiant glow without a sticky or heavy residue."
                 }
@@ -26,19 +26,19 @@ var ecommerce = {
             "Nivea": {
                 "lipbalm":{
                     name:"Caring lip balm",
-                    src:".img/nivea lipbalm.jpg",
+                    src:"img/nivea lipbalm.jpg",
                     price:"600",
                     description:" provide long-lasting, 24-hour hydration and nourishment, typically formulated with natural oils, shea butter, and no mineral oils to keep lips smooth and protected."
                 },
                 "lotion": {
                     name: "extra white body lotion",
-                    src: ".img/InstantGlow body lotion nivea.webp",
+                    src: "img/InstantGlow body lotion nivea.webp",
                     price: "1500",
                     description: "Designed to brighten and moisturize, often featuring niacinamide."
                 },
                 "cream": {
                     name: "Nivea face cream",
-                    src: ".img/nivea cream.webp",
+                    src: "img/nivea cream.webp",
                     price: "100",
                     description: "thin moisturizing cream"
                 }
@@ -46,19 +46,19 @@ var ecommerce = {
             "Saeed Ghani": {
                 "Face wash": {
                     name: "Vitamin C FaceWash",
-                    src: ".img/saeedghani facewash.jpg",
+                    src: "img/saeedghani facewash.jpg",
                     price:"500",
                     description: " A popular, gentle formula that brightens skin, reduces dark spots, and fights fine lines."
                 },
                 "Body lotion": {
                     name: "Body Lotion",
-                    src: ".img/saeedghani lotion.jpg",
+                    src: "img/saeedghani lotion.jpg",
                     price: "800",
                     description: "Formulated for different skin needs, ranging from lightweight daily lotions (Vitamin C/SPF 30) to thick, hydrating creams for dry skin and deep-moisture shea butter for winter."
                 },
                 "ubtan":{
                     name:"ubtan",
-                    src:".img/SpecialUbgtan.webp",
+                    src:"img/SpecialUbgtan.webp",
                     price:"405",
                     description:"Natural Glow & Whitening Formulated to reduce dark spots and provide a brighter, radiant, and younger-looking skin tone.",
                 }
@@ -67,19 +67,19 @@ var ecommerce = {
             "Dermacos": {
                 "Face Cleanser": {
                     name: "Dermacos Dermapure Hydrating Cooling Cucumber Cleanser",
-                    src: ".img/Dermacos_Dermapure_Hydrating_Cooling_Cucumber_Cleanser.webp",
+                    src: "img/Dermacos_Dermapure_Hydrating_Cooling_Cucumber_Cleanser.webp",
                     price:"800",
                     description: "A Cooling Cucumber Cleanser formulated with natural cucumber extracts for a calming effect. It is a gentle, soothing cleanser that removes makeup and surface dirt while providing an instant, hydrating, and cooling sensation."
                 },
                 "Face Scrub": {
                     name: "Dermacos Luster Kiwi Scrub: ",
-                    src: ".img/dermacos scrub.jpg",
+                    src: "img/dermacos scrub.jpg",
                     price: "900",
                     description: " A refreshing formula that uses micro-exfoliating beads and kiwi extract to remove dirt, excess oil, and impurities, helping to clear blackheads and refine skin."
                 },
                 "Cream":{
                     name:"massaging cream",
-                    src:".img/dermacos cream.webp",
+                    src:"img/dermacos cream.webp",
                     price:"1000",
                     description:" Infused with vitamins A, C, and E to provide deep nourishment, combat free radicals, and improve skin elasticity."
                 }
@@ -88,19 +88,19 @@ var ecommerce = {
             "Organic Traveler": {
                 serum: {
                     name: "serum",
-                    src: ".img/organictrav serum.webp",
+                    src: "img/organictrav serum.webp",
                     price: "1500",
                     description: " targeted, clean skincare serums in Pakistan designed for various skin concerns, including acne, aging, and dullness"
                 },
                 moisturizer: {
                     name: "Rapid Repair Moisturizer",
-                    src: ".img/organic traveller most .webp",
+                    src: "img/organic traveller most .webp",
                     price: "1999",
                     description: "the Rapid Repair Skin Repairing Moisturiser for barrier support."
                 },
                 sunblock:{
                     name:"OrganicTraveller BarrierHydrate Sunscreen",
-                    src:".img/OrganicTraveller-BarrierHydrateSunscreen01.webp",
+                    src:"img/OrganicTraveller-BarrierHydrateSunscreen01.webp",
                     price:"2000",
                     description:"A hybrid formula designed with ceramides and hyaluronic acid to strengthen the skin barrier while providing broad-spectrum protection."
                 }
@@ -110,19 +110,19 @@ var ecommerce = {
             Sunsilk: {
                 shampoo: {
                     name: "Sunsilk Black Shine Shampoo",
-                    src: ".img/sunsilk shampoo.avif",
+                    src: "img/sunsilk shampoo.avif",
                     price: "2500",
                     description: " Features a Super-Mix formula with Henna, Pearl Protein, and Vitamin E to nourish hair, enhancing its natural black shine and strength."
                 },
                 conditioner: {
                     name: "Brightening conditioner",
-                    src: ".img/sunsilk conditioner.avif",
+                    src: "img/sunsilk conditioner.avif",
                     price: "1999",
                     description: "Formulated with amla pearl extract and Vitamin E to enhance natural shine, fight dullness, and provide nourishment."
                 },
                 serum:{
                     name:"Sunsilk serum",
-                    src:".img/sunsilk serum.webp",
+                    src:"img/sunsilk serum.webp",
                     price:"3000",
                     description:" a lightweight, non-sticky hair styling product designed to tame frizz and add shine for up to 48 hours"
                 }
@@ -133,19 +133,19 @@ Clothes: {
             "T-Shirts": {
                 zellbury: {
                     name: "Zellbury Women's Oversized T-Shirt",
-                    src: ".img/zellbury tshirt.jpg",
+                    src: "img/zellbury tshirt.jpg",
                     price: "2999",
                     description: "Relaxed fit, soft jersey cotton t-shirt. Perfect for lounging or street style."
                 },
                 outfitters: {
                     name: "Outfitters Women's Cropped T-Shirt",
-                    src: ".img/outfitters tshirt.webp",
+                    src: "img/outfitters tshirt.webp",
                     price: "1500",
                     description: "Trendy cropped fit with round neck. Pairs well with high-waisted jeans."
                 },
                 furor: {
                     name: "Furor Women's Printed T-Shirt",
-                    src: ".img/furor tshirt.jpg",
+                    src: "img/furor tshirt.jpg",
                     price: "1700",
                     description: "Vibrant floral print t-shirt. Casual and chic."
                 }
@@ -153,19 +153,19 @@ Clothes: {
             "Pants": {
                  zellbury: {
                     name: " Women's zellbury pant",
-                    src: ".img/zellbury pant.webp",
+                    src: "img/zellbury pant.webp",
                     price: "2999",
                     description: " Features include skinny fits, wide-leg, and trendy flared styles."
                 },
                 outfitters: {
                     name: " Women's outfitters pant",
-                    src: ".img/outfitters pant.webp",
+                    src: "img/outfitters pant.webp",
                     price: "1500",
                     description: "modern, trendy, and comfortable styles"
                 },
                 furor: {
                     name: "Furor Women's pant",
-                    src: ".img/FWBT24-028_10.webp",
+                    src: "img/FWBT24-028_10.webp",
                     price: "1700",
                     description: " A trendy, affordable selection of bottoms, including skinny jeans, high-waisted trousers, cargo pants, and jogger pants designed for comfort and style"
                 }
@@ -173,19 +173,19 @@ Clothes: {
             " 3-piece ":{
                 Limelight: {
                     name: "Limelight Women's Lawn Suit",
-                    src: ".img/Limelight dress.jpg",
+                    src: "img/Limelight dress.jpg",
                     price: "13000",
                     description: "Premium lawn fabric with intricate embroidery. Includes dupatta."
                 },
                 Nishat: {
                     name: "Nishat Luxury Embroidered Suit",
-                    src: ".img/Nishat Linen dress.jpg",
+                    src: "img/Nishat Linen dress.jpg",
                     price: "18999",
                     description: "Simple embroidery work with chiffon dupatta."
                 },
                 Sapphire: {
                     name: "Sapphire Printed Dress for women",
-                    src: ".img/Sapphire dress.webp",
+                    src: "img/Sapphire dress.webp",
                     price: "8000",
                     description: "Dark blue printed dress with cambric shalwar."
                 }
@@ -193,19 +193,19 @@ Clothes: {
             " Bridal Dresses":{
                  "Sana Yasir's": {
                     name: "Sana Yasir bridal Dress",
-                    src: ".img/Sana Yasir bridal.jpg",
+                    src: "img/Sana Yasir bridal.jpg",
                     price: "90000",
                     description: " Expertly handcrafted details featuring zardozi, dabka, sequins, stones, and pearl work."
                 },
                 "Asim Jofa": {
                     name: "Asim Jofa bridal Dress",
-                    src: ".img/Asim Jofa bridal.jpg",
+                    src: "img/Asim Jofa bridal.jpg",
                     price: "120000",
                     description: "luxurious, bespoke Pakistani wedding wear, blending traditional, intricate zardozi, tilla, and pearl embroidery with modern, elegant silhouettes"
                 },
                 " Sadaf Fawad Khan":{
                     name: "Sadaf Fawad Khan bridal Dress",
-                    src: ".img/Sadaf Fawad Khan bridal.webp",
+                    src: "img/Sadaf Fawad Khan bridal.webp",
                     price: "50000",
                     description: " Beautiful Bridal Garara By Sadaf Fawad Khan "
                 }
@@ -217,19 +217,19 @@ Clothes: {
             shoes: {
                 Bata: {
                     name: "Bata shoes for women",
-                    src: ".img/bata shoes.jpg",
+                    src: "img/bata shoes.jpg",
                     price: "2500",
                     description: "Comfortable Shoes for women"
                 },
                 stylo: {
                     name: "Stylo Women's Shoes",
-                    src: ".img/stylo shoes.webp",
+                    src: "img/stylo shoes.webp",
                     price: "3000",
                     description: "Comfortable and durable options in all colors"
                 },
                 servis: {
                     name: "Servis regural wear pumps",
-                    src: ".img/servis shoes.webp",
+                    src: "img/servis shoes.webp",
                     price: "4999",
                     description: " featuring sparkly and plain designs pumps"
                 }
@@ -237,19 +237,19 @@ Clothes: {
             Slippers: {
                 bata: {
                     name: "Bata Women's slipper",
-                    src: ".img/bata slipper.jpg",
+                    src: "img/bata slipper.jpg",
                     price: "1299",
                     description: "Simple and durable daily wear slipper."
                 },
                 service: {
                     name: "Service Women's Comfort Slipper",
-                    src: ".img/servis slipper.webp",
+                    src: "img/servis slipper.webp",
                     price: "1600",
                     description: "Cushioned footbed with soft straps."
                 },
                 stylo: {
                     name: "Stylo Women's Fashion Slipper",
-                    src: ".img/stylo slipper.jpg",
+                    src: "img/stylo slipper.jpg",
                     price: "1700",
                     description: "Trendy design with comfort"
                 }
@@ -260,25 +260,25 @@ Clothes: {
     Accessories: {
         Earrings: {
             name: "Gold Plated simple Earrings",
-            src: ".img/earing.jpg",
+            src: "img/earing.jpg",
             price: "1599",
             description: "Traditional simple and minimal design with gold plating. "
         },
         Necklace: {
             name: "3 Layered Necklace Set",
-            src: ".img/3_Layered_Necklace.jpg",
+            src: "img/3_Layered_Necklace.jpg",
             price: "5000",
             description: "Elegant gold-toned 3 layered necklace."
         },
         rings: {
             name: "Simple and elegant ring",
-            src: ".img/ring.jpg",
+            src: "img/ring.jpg",
             price: "1000",
             description: "Shiny, Simple and elegant ring"
         },
         watch:{
             name:"Simple white watch",
-            src:".img/watch.jpg",
+            src:"img/watch.jpg",
             price:"999",
             description:"Simple white watch for girls for everyday use"
         }
